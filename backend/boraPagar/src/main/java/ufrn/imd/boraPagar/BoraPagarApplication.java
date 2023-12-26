4feb22d33fb6db19aa84c995b6a07944f0ca88e1
Kywal <kywalfilho@outlook.com>
2023-12-26 18:35:05 -0300

    Add spring project and dockerfile

---
+++ backend/boraPagar/src/main/java/ufrn/imd/boraPagar/BoraPagarApplication.java
@@ -0,0 +1,13 @@
+package ufrn.imd.boraPagar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoraPagarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoraPagarApplication.class, args);
+	}
+
+}
