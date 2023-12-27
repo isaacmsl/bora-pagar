cc0daa5a423a01d85c9615f7ca5df143ad486e72
Ian Gabriel <iangabriel.dias12@gmail.com>
2023-12-27 20:23:52 -0300

    feat: Adding subject list

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,13 +2,17 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import { createVuetify } from 'vuetify';
 
 const app = createApp(App)
+const vuetify = createVuetify({
+  theme: false
+});
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
