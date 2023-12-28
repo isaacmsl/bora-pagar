304203fd61c5f3536ba9cbdaf6fc87a64de4ba6a
DanilloFonseca <danillo.fonseca.129@ufrn.edu.br>
2023-12-27 21:47:53 -0300

    feat: new pagination

--- frontend/src/views/SubjectList.vue
+++ frontend/src/views/SubjectList.vue
@@ -2,6 +2,10 @@
 import {VList} from "vuetify/components/VList";
 import {VPagination} from "vuetify/components/VPagination";
 import SubjectListItem from "@/components/SubjectListItem.vue";
+import {ref} from "vue";
+const page = ref(1);
+const qntSubjectsOnPage = 4;
+const qntVisiblePages = 7;
 
 const subjects = [
   {
@@ -18,23 +22,59 @@ const subjects = [
     code: "IMD0020",
     name: "Fundamentos matemáticos da computação I",
     department: "Instituto metrópole digital"
+  },
+  {
+    code: "IMD00X0",
+    name: "Natação I",
+    department: "Departamento de Educação Física"
+  },
+  {
+    code: "IMD00X0",
+    name: "Forró I",
+    department: "Departamento de Dança"
+  },
+  {
+    code: "IMD00X0",
+    name: "Flauta I",
+    department: "Departamento de Música"
+  },
+  {
+    code: "IMD00X0",
+    name: "Basquete I",
+    department: "Departamento de Educação Física"
+  },
+  {
+    code: "IMD00X0",
+    name: "Axé I",
+    department: "Departamento de Música"
+  },
+  {
+    code: "IMD00X0",
+    name: "Introdução a Lógica",
+    department: "Departamento de Informática e Matemática Aplicada"
   }
 ]
+
+const qntPages = Math.ceil(subjects.length / qntSubjectsOnPage);
 </script>
 
 <template>
   <main class="container">
-    <h1>Listar disciplinas</h1>
+    <h1>Listar disciplinas {{ page }}</h1>
     <v-list class="list">
       <SubjectListItem 
-        v-for="subject in subjects"
+        v-for="subject in subjects.slice(qntSubjectsOnPage*(page-1), qntSubjectsOnPage*page)"
         :key="subject.code"
         :code="subject.code"
         :department="subject.department"
         :name="subject.name"
       />
     </v-list>
-    <v-pagination />
+    <v-pagination 
+      :length="qntPages"
+      v-model="page"
+      :total-visible="qntVisiblePages"
+    ></v-pagination>
   </main>
 </template>
 
