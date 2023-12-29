d3457348c9dd5b30c13b1bd7b23770bea5e65da3
RamonJales <ramonjales123@gmail.com>
2023-12-28 22:26:33 -0300

    feat: SubjectRepository created

---
+++ backend/src/main/java/ufrn/imd/boraPagar/repositories/SubjectRepository.java
@@ -0,0 +1,5 @@
+package ufrn.imd.boraPagar.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubjectRepository extends JpaRepository<SubjectModel, Integer> {}
