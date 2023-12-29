8d2cd5ba88be8414fee286936f586e90688fbc7d
Isaac Lourenço <isaac.lourenco.704@ufrn.edu.br>
2023-12-29 11:06:39 -0300

    feat: Subject entity using Abstract entity

--- backend/src/main/java/ufrn/imd/boraPagar/repositories/SubjectRepository.java
+++
@@ -1,5 +0,0 @@
-package ufrn.imd.boraPagar.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubjectRepository extends JpaRepository<SubjectModel, Integer> {}
---
+++ backend/src/main/java/ufrn/imd/boraPagar/subject/SubjectRepository.java
@@ -0,0 +1,16 @@
+package ufrn.imd.boraPagar.subject;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import ufrn.imd.boraPagar.core.AbstractRepository;
+
+@Repository
+public interface SubjectRepository extends AbstractRepository<SubjectModel> {
+    SubjectModel findByComponentID(int componentID);
+    SubjectModel findByName(String name);
+    SubjectModel findByCode(String code);
+    List<SubjectModel> findAllByTotalHours(int totalHours);
+    List<SubjectModel> findAllByDepartment(String department);
+}
