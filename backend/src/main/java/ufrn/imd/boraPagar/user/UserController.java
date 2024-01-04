dda8117d9f6dcdb85aaa8c91c6283f41ec1cb6fc
Isaac Lourenço <isaac.lourenco.704@ufrn.edu.br>
2024-01-03 21:51:06 -0300

    fix: remove unecessary import

--- backend/src/main/java/ufrn/imd/boraPagar/user/UserController.java
+++ backend/src/main/java/ufrn/imd/boraPagar/user/UserController.java
@@ -1,7 +1,5 @@
 package ufrn.imd.boraPagar.user;
 
-import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
