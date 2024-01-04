696d7ddb5b4a3f22071e405d5049fd7ea1a68566
Isaac Lourenço <isaac.lourenco.704@ufrn.edu.br>
2024-01-03 21:51:06 -0300

    fix: remove unecessary import

--- backend/src/main/java/ufrn/imd/boraPagar/user/UserService.java
+++ backend/src/main/java/ufrn/imd/boraPagar/user/UserService.java
@@ -1,7 +1,5 @@
 package ufrn.imd.boraPagar.user;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.stereotype.Service;
