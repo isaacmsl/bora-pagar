0b2b129ebd9a456dee2569be6db6e1c3b8fce475
Danillo Fonseca <127620804+DanilloFonseca@users.noreply.github.com>
2024-01-07 11:16:30 -0300

    feat: credencial persists local storage (#47)
    * feat: credencial persists local storage
    
    * feat: remove credential localStorage logout
    
    * feat: logout after error decodeCredential
    
    ---------
    
    Co-authored-by: Isaac Lourenço <isaac.lourenco.704@ufrn.edu.br>

--- frontend/src/views/SubjectList.vue
+++ frontend/src/views/SubjectList.vue
@@ -10,6 +10,7 @@ import UserProfile from '@/components/UserProfile.vue';
 const page = ref(1);
 const qntSubjectsOnPage = 4;
 const qntVisiblePages = 7;
+const credentialKeyName = 'credential';
 
 const loggedIn = ref(false);
 const user = ref<GoogleUserInfo>();
@@ -18,14 +19,30 @@ function normalizeGivenName(name : String) {
   return name.charAt(0) + name.slice(1).toLowerCase();
 }
 
+function getCredentialFromLocalStorage() {
+  const credential = localStorage.getItem(credentialKeyName);
+  if (credential) {
+    try {
+      user.value = decodeCredential(credential) as GoogleUserInfo;
+      loggedIn.value = true;
+      user.value.given_name = normalizeGivenName(user.value.given_name);
+    } catch (error) {
+      logout();
+    }
+  }
+}
+
+getCredentialFromLocalStorage();
+
 function googleLoginCallback(response : any) {
-  console.log(decodeCredential(response.credential));
+  localStorage.setItem(credentialKeyName, response.credential)
   loggedIn.value = true;
   user.value = decodeCredential(response.credential) as GoogleUserInfo;
   user.value.given_name = normalizeGivenName(user.value.given_name);
 }
 
 function logout() {
+  localStorage.removeItem(credentialKeyName);
   loggedIn.value = false;
   googleLogout();
 }
