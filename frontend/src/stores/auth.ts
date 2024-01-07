b43a647c1ac0dca9a798b2c78fa8565839a4e556
Isaac Lourenço <isaac.lourenco.704@ufrn.edu.br>
2024-01-07 17:19:54 -0300

    feat: auth store pinia

---
+++ frontend/src/stores/auth.ts
@@ -0,0 +1,43 @@
+import type { GoogleUserInfo } from '@/types/GoogleUserInfo';
+import { defineStore } from 'pinia';
+import { decodeCredential, googleLogout } from 'vue3-google-login';
+
+const credentialKeyName = 'credential';
+
+function normalizeGivenName(name : String) {
+  return name.charAt(0) + name.slice(1).toLowerCase();
+}
+
+export const useAuthStore = defineStore('auth', {
+  state: () => {
+    return { user: undefined as GoogleUserInfo | undefined }
+  },
+  actions: {
+    setUserFromCredential(credential : string) {
+      this.user = decodeCredential(credential) as GoogleUserInfo;
+      this.user.given_name = normalizeGivenName(this.user.given_name);
+    },
+    googleLoginCallback(response : any) {
+      localStorage.setItem(credentialKeyName, response.credential);
+      this.setUserFromCredential(response.credential);
+    },
+    getCredentialFromLocalStorage() {
+      const credential = localStorage.getItem(credentialKeyName);
+      if (credential) {
+        try {
+          this.setUserFromCredential(credential);
+        } catch (error) {
+          this.logout();
+        }
+      }
+    },
+    logout() {
+      localStorage.removeItem(credentialKeyName);
+      this.user = undefined;
+      googleLogout();
+    },
+    loggedIn() {
+      return this.user != undefined;
+    }
+  },
+})
