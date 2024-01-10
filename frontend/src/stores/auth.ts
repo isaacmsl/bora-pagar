d2a3012642d0e7d81e5ca83d47d5c1b86597f52f
Isaac Lourenço <isaac.lourenco.704@ufrn.edu.br>
2024-01-10 14:18:11 -0300

    fix: tmp isCredentialNotExpired always true

--- frontend/src/stores/auth.ts
+++ frontend/src/stores/auth.ts
@@ -18,8 +18,7 @@ export const useAuthStore = defineStore('auth', {
   },
   actions: {
     isCredentialNotExpired(credential : string) {
-      const user = decodeCredential(credential) as GoogleUserInfo;
-      return Number(user.exp) > Date.now(); 
+      return true;
     },
     setUserFromCredential(credential : string) {
       this.user = decodeCredential(credential) as GoogleUserInfo;
