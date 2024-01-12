c40d4d06e2c8c23cf13aaa984c2df12ca66953fb
Isaac Lourenço <isaac.lourenco.704@ufrn.edu.br>
2024-01-12 16:53:58 -0300

    fix: username?.trim() UserService frontend (#162)

--- frontend/src/services/UserService.ts
+++ frontend/src/services/UserService.ts
@@ -33,7 +33,7 @@ export class UserService {
   public async searchUsersByUsername(page : number, username? : string) : Promise<PageUser>{
     const response = await this.axiosInstance.get<any, AxiosResponse<PageUser>>('', {
       params: {
-        partialUsername: username.trim() || '',
+        partialUsername: username?.trim() || '',
         page
       }
     });
