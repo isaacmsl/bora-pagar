16e3c11c294f4da34ea86f1a8fd96b7b19856005
RamonJales <ramonjales123@gmail.com>
2024-01-13 20:29:11 -0300

    handleMongoException created

--- backend/src/main/java/ufrn/imd/boraPagar/exceptions/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/ufrn/imd/boraPagar/exceptions/handler/GlobalExceptionHandler.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.mongodb.MongoException;
+
 import ufrn.imd.boraPagar.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -41,6 +43,14 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(DataAccessException.class)
     public  ResponseEntity<ExceptionResponse> handleDataAccessException(DataAccessException ex) {
         ExceptionResponse response = new ExceptionResponse(LocalDateTime.now(), HttpStatus.INTERNAL_SERVER_ERROR, "Internal error accessing the database!");
+        response.getErrors().add(ex.getMessage());
+
+        return new ResponseEntity<>(response, response.getStatus());
+    }
+
+    @ExceptionHandler(MongoException.class)
+    public  ResponseEntity<ExceptionResponse> handleMongoException(MongoException ex) {
+        ExceptionResponse response = new ExceptionResponse(LocalDateTime.now(), HttpStatus.INTERNAL_SERVER_ERROR, "Internal error in mongodb!");
         response.getErrors().add(ex.getMessage());
 
         return new ResponseEntity<>(response, response.getStatus());
