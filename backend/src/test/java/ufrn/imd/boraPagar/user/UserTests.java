28ece006b18250ae7f002a22632a9a2688b7b80d
RamonJales <ramonjales123@gmail.com>
2024-01-13 22:41:09 -0300

    add assert verifications in shouldPageZeroHasLengthTwo

--- backend/src/test/java/ufrn/imd/boraPagar/user/UserTests.java
+++ backend/src/test/java/ufrn/imd/boraPagar/user/UserTests.java
@@ -22,6 +22,8 @@ public class UserTests {
     UserModel user;
     UserModel user2;
 
+    private int pageSize = 5;
+
     @Before
     public void setUp() throws Exception {
         user = new UserModel();
@@ -77,7 +79,7 @@ public class UserTests {
     public void shouldFindAllByName() {
         String partialName = "IsAaC";
         
-        Pageable pageable = PageRequest.of(0, 2);
+        Pageable pageable = PageRequest.of(0, pageSize);
         Page<UserModel> userPage = repository.findAllByName(pageable, partialName);
         
         Assert.assertTrue(userPage.getContent().contains(user));
@@ -87,7 +89,7 @@ public class UserTests {
     public void shouldNotFindAllByName() {
         String partialName = "joao";
         
-        Pageable pageable = PageRequest.of(0, 2);
+        Pageable pageable = PageRequest.of(0, pageSize);
         Page<UserModel> userPage = repository.findAllByName(pageable, partialName);
         
         Assert.assertTrue(userPage.getContent().isEmpty());
@@ -97,7 +99,7 @@ public class UserTests {
     public void shouldFindAllByUsername() {
         String partialName = "IsAaC";
         
-        Pageable pageable = PageRequest.of(0, 2);
+        Pageable pageable = PageRequest.of(0, pageSize);
         Page<UserModel> userPage = repository.findAllByUsername(pageable, partialName);
 
         Assert.assertTrue(userPage.getContent().contains(user));
@@ -107,7 +109,7 @@ public class UserTests {
     public void shouldNotFindAllByUsername() {
         String partialName = "joao";
         
-        Pageable pageable = PageRequest.of(0, 2);
+        Pageable pageable = PageRequest.of(0, pageSize);
         Page<UserModel> userPage = repository.findAllByUsername(pageable, partialName);
 
         Assert.assertTrue(userPage.getContent().isEmpty());
@@ -120,15 +122,17 @@ public class UserTests {
 
     @Test
     public void shouldPageZeroHasLengthTwo() {
-        Pageable pageable = PageRequest.of(0, 5);
+        Pageable pageable = PageRequest.of(0, pageSize);
         Page<UserModel> userPage = repository.findAllActiveByPage(pageable);
 
         Assert.assertEquals(2, userPage.getContent().size());
+        Assert.assertTrue(userPage.getContent().contains(user));
+        Assert.assertTrue(userPage.getContent().contains(user2));
     }
 
     @Test
     public void shouldPageOneHasLengthZero() {
-        Pageable pageable = PageRequest.of(1, 2);
+        Pageable pageable = PageRequest.of(1, pageSize);
         Page<UserModel> userPage = repository.findAllActiveByPage(pageable);
 
         Assert.assertEquals(0, userPage.getContent().size());
@@ -136,7 +140,7 @@ public class UserTests {
 
     @Test
     public void shouldPageZeroHasUserAsFirstObjectUsingOrderByName() {
-        Pageable pageable = PageRequest.of(0, 5);
+        Pageable pageable = PageRequest.of(0, pageSize);
         Page<UserModel> userPage = repository.findAllByNameOrderByNameAsc(pageable);
 
         Assert.assertEquals(user, userPage.getContent().get(0));
@@ -145,7 +149,7 @@ public class UserTests {
 
     @Test
     public void shouldPageZeroHasUserAsFirstObjectUsingOrderByUsername() {
-        Pageable pageable = PageRequest.of(0, 5);
+        Pageable pageable = PageRequest.of(0, pageSize);
         Page<UserModel> userPage = repository.findAllByUsernameOrderByUsernameAsc(pageable);
 
         Assert.assertEquals(user, userPage.getContent().get(0));
